b616160a15855abe206f4c5b2a034622db8dbe6f
Stella Chung <stll.chung@gmail.com>
2020-09-01 23:21:06 -0400

    messing with config for featured works

--- .eleventy.js
+++ .eleventy.js
@@ -37,10 +37,21 @@ module.exports = function (eleventyConfig) {
     });
   });
 
-  eleventyConfig.addCollection("featuredWork", function (collectionApi) {
-    const results = collectionApi.getFilteredByTags("work");
+  eleventyConfig.addCollection("allStuff", function (collectionApi) {
+    const results = collectionApi.getFilteredByTags("post");
+    console.log("post", results[0]);
+
     const appearances = [...results];
     return appearances.sort(function (a, b) {
+      return a.data.order - b.data.order;
+    });
+  });
+
+  eleventyConfig.addCollection("featuredWork", function (collectionApi) {
+    const results = collectionApi.getFilteredByTags("work");
+    const works = [...results];
+    // console.log("works", works);
+    return works.sort(function (a, b) {
       return a.data.order - b.data.order;
     });
   });
--- works/the-philadelphia-inquirer.md
+++ works/the-philadelphia-inquirer.md
@@ -3,6 +3,7 @@ name: The Philadelphia Inquirer
 img: /static/img/philadelphia-inquirer.svg
 link: https://www.inquirer.com/author/echeverri_sebastian/
 order: 1
+summary: Staff Writer, AAAS Mass Media Fellow & contributing photographer. Wrote for Inquirer’s Health & Science desk, with an average of 7,550 online readers.
 ---
 
 Staff Writer, AAAS Mass Media Fellow & contributing photographer. Wrote for Inquirer’s Health & Science desk, with an average of 7,550 online readers.
